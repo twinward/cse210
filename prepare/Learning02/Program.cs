5c724c0e295764b67a8fcc68723db293eb557645
twinward <win18005@byui.edu>
2023-11-08 15:52:02 -0700

    prepare02 done;  team activity done (class design)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Youth Counselor";
+        job1._company = "FSY";
+        job1._startYear = 2023;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sandwich Artist";
+        job2._company = "Subway";
+        job2._startYear = 2022;
+        job2._endYear = 2022;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Tanner Winward";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+    public class Prompt
+        {
+            // Attributes
+            public List<Prompt> _prompts = new List<Prompt>();
+
+            //Behaviors
+            public void Display() //Find *random* prompt
+            {
+                foreach (Prompt prompt in _prompts)
+                {
+                    prompt.Display();
+                }
+            }
+        }
+
+    public class Entry
+        {
+            // Attributes
+            public string _date;
+            public string _prompt;
+            public string _entry;
+
+            /* Behaviors
+                1, GET prompt (from Prompt class)
+                2, GET user input / store
+                3, GET date
+            */
+        }
+
+    public class Journal
+    {
+        // Attributes
+        public string _fileName;
+        public List<Entry> _jobs = new List<Entry>();
+        
+        /* Behaviors
+            PUT journal (iterate through entries)
+            LOAD journal
+        */
+
+    }
+
     }
 }
+
